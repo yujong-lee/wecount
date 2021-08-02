1566e5fa9d0b1bd77794ac1ee095f7ddac4eb383
Hyun <48207131+KoreanThinker@users.noreply.github.com>
2021-08-02 20:00:34 +0900

    [Modal] UI component Implementation  (#14)
    Basic [Modal] implementation.

---
+++ src/components/uis/Modal.svelte
@@ -0,0 +1,35 @@
+<style>
+  .backdrop {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.8);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
+
+<script>
+  import {createEventDispatcher} from 'svelte';
+
+  export let visible;
+  export let style = '';
+  export let disableBackdropClickToClose = false;
+
+  const dispatch = createEventDispatcher();
+
+  const handleBackdropClick = () => {
+    if(disableBackdropClickToClose) return;
+ 
+   dispatch('close');
+  };
+</script>
+
+{#if visible}
+  <div class="backdrop" style={style} on:click|self={handleBackdropClick}>
+    <slot />
+  </div>
+{/if}
