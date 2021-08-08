3697360c161d0cb363602d5d2dd6e3569d97fe0a
Hyun <48207131+KoreanThinker@users.noreply.github.com>
2021-08-08 16:01:16 +0900

    Implment MemberList (#41)
    * Initialize basic select and add arrow-down icon
    
    * Initialize MemberList.svelte with basic css & i18n
    
    * Import Member Screen TrashCan Icon
    
    * Import MemberScreen badge-crown icon
    
    * MemberList connect onMemberChange & onMemberDelete event
    
    * MemberList implement Infinity scroll
    
    * Update: MemberList background color connect to theme
    
    * Add new line before paragraph
      - Based on review https://github.com/wecount-dev/wecount/pull/41#discussion_r684723987

--- src/utils/Icon.ts
+++ src/utils/Icon.ts
@@ -1,11 +1,15 @@
 import svgArrowDown from '../../assets/svgs/arrow-down.svg';
+import svgBadgeCrown from '../../assets/svgs/badge-crown.svg';
 import svgCheck from '../../assets/svgs/check.svg';
 import svgKey from '../../assets/svgs/key.svg';
 import svgLogo from '../../assets/svgs/logo.svg';
 import svgMail from '../../assets/svgs/mail.svg';
+import svgTrashCan from '../../assets/svgs/trash-can.svg';
 
 export const SvgCheck = svgCheck;
 export const SvgKey = svgKey;
 export const SvgLogo = svgLogo;
 export const SvgMail = svgMail;
 export const SvgArrowDown = svgArrowDown;
+export const SvgTrashcan = svgTrashCan;
+export const SvgBadgeCrown = svgBadgeCrown;
